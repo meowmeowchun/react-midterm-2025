195f8b34b9a9d52af22e7efefe08af324dc503d4
x1jia-1 <s111219015@stu.ntue.edu.tw>
2025-04-14 12:50:04 +0800

    add cart icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import { MdShoppingCart } from "react-icons/md";
 
 function Header() {
   const location = useLocation();
@@ -11,12 +12,26 @@ function Header() {
   ]; // navbar links
 
   return (
-    <header className="bg-neutral">
-      <div className="flex justify-center py-4">
+    <header className="bg-neutral px-4">
+    {/* Logo + Cart */}
+    <div className="relative py-4">
+      {/* Logo：居中 */}
+      <div className="flex justify-center">
         <Link to="/">
           <img src="./img/icon choas 4.png" className="h-20" alt="Logo" />
         </Link>
       </div>
+
+      {/* Cart Icon：右上角 */}
+      <Link
+  to="/cart"
+  className="absolute right-0 top-1/2 -translate-y-1/2 text-red-600 text-3xl hover:text-primary"
+>
+  <MdShoppingCart />
+</Link>
+
+    </div>
+
 
       <nav className="shadow-md">
         <ul className="flex justify-center gap-8 py-4">
