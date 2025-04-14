ac5b0a683e1c0766a30bafba14e432db51814144
x1jia-1 <s111219015@stu.ntue.edu.tw>
2025-04-14 13:21:26 +0800

    fake cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} /> {/* ← 新增 */}
       </Routes>
     </Router>
   );
