c657c57cb9a439e8f1b9d09e1725fb33d68b8a4b
Meowmeowchun <meowmeowchun@gmail.com>
2025-04-18 03:56:05 +0800

    adjust cover head

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function Cover() {
   return (
-    <div className="h-[100vh] bg-neutral flex items-center justify-center"> 
+    <div className="h-[100vh] bg-neutral flex items-center justify-center relative"> 
       <Link to="/" className="flex gap-4 w-full justify-center items-center">
-        <img src="./img/left head.png" className="h-[60vh] absolute left-0" alt="Left Head" />
+        <img src="./img/left head.png" className="h-[50vh] absolute left-0 top-20" alt="Left Head" />
         <img src="./img/main head.png" className="h-[80vh]" alt="Main Head" />
-        <img src="./img/right head.png" className="h-[60vh] absolute right-0" alt="Right Head" />
+        <img src="./img/right head.png" className="h-[50vh] absolute right-0 top-20" alt="Right Head" />
       </Link>
     </div>
   );
